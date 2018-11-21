a25dd2ec934a4157f58f4f61b957d6834f898ec9
Lê Hoàng Vũ <elhoangvu@gmail.com>
2018-11-21 15:52:10 +0700

    Initial Commit

---
+++ Phuot63/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Phuot63
+//
+//  Created by Le Hoang Vu on 11/21/18.
+//  Copyright © 2018 Le Hoang Vu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Phuot63/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Phuot63
+//
+//  Created by Le Hoang Vu on 11/21/18.
+//  Copyright © 2018 Le Hoang Vu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
